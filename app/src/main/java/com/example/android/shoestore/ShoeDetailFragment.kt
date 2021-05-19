452529d38ddd9e9e6c1de7c5e5b205eca6a54449
Dave Roye <daveroye@gmail.com>
2021-05-19 04:30:51 -0500

    move shoe detail data handling between layout and view model and out of display fragment

--- app/src/main/java/com/example/android/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/example/android/shoestore/ShoeDetailFragment.kt
@@ -10,7 +10,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.example.android.shoestore.databinding.FragmentShoedetailBinding
-import com.example.android.shoestore.models.Shoe
 
 class ShoeDetailFragment : Fragment() {
 
@@ -21,34 +20,29 @@ class ShoeDetailFragment : Fragment() {
 
         val binding: FragmentShoedetailBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_shoedetail, container, false)
+        // needed for view model data binding to layout
+        binding.shoesViewModel = viewModel
+        // attach shoe data model as well to the layout data binding
+        binding.aShoe = viewModel.aShoe
+        // make data binding lifecycle aware
+        binding.lifecycleOwner = this
 
-        binding.saveShoeInfo.setOnClickListener {
-            val shoeName = binding.shoeName.text.toString()
-            val shoeSize = binding.shoeSize.text.toString()
-            val shoeCompany = binding.shoeCompany.text.toString()
-            val shoeDescription = binding.shoeDescription.text.toString()
-
-            if (shoeName.isBlank() || shoeSize.isBlank() ||
-                shoeCompany.isBlank() || shoeDescription.isBlank() ) {
-                Toast.makeText( requireContext(),
-                           "Please fill in all fields",
-                                Toast.LENGTH_SHORT).show()
-            } else {
-                // create a new shoe from entered data
-                val newShoe = Shoe( shoeName, shoeSize.toDouble(),
-                                    shoeCompany, shoeDescription )
-                // add the new show to the view model data object
-                viewModel.addShoeToList(newShoe)
+        viewModel.showToast.observe(viewLifecycleOwner, { showToast ->
+            if (showToast) {
+                Toast.makeText(
+                    requireContext(),
+                    "Please fill in all fields",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+        viewModel.shoeComplete.observe(viewLifecycleOwner, { isComplete ->
+            if (isComplete) {
                 // return to previous view
                 requireView().findNavController().popBackStack()
+                viewModel.clearShoe()
             }
-        }
-
-        binding.cancelSave.setOnClickListener {
-            // return to previous view without saving shoe data
-            requireView().findNavController().popBackStack()
-        }
-
+        })
         return binding.root
     }
 }
--- app/src/main/java/com/example/android/shoestore/ShoesViewModel.kt
+++ app/src/main/java/com/example/android/shoestore/ShoesViewModel.kt
@@ -12,12 +12,45 @@ class ShoesViewModel : ViewModel() {
     val shoeList : LiveData<List<Shoe>>
         get() = _shoeList
 
+    private val _showToast = MutableLiveData<Boolean>()
+    val showToast : LiveData<Boolean>
+        get() = _showToast
+
+    private val _shoeComplete = MutableLiveData<Boolean>()
+    val shoeComplete : LiveData<Boolean>
+        get() = _shoeComplete
+
     private val aShoeList = ArrayList<Shoe>()
 
-    fun addShoeToList(newShoe : Shoe) {
-        // add new shoe to the list
-        aShoeList.add(newShoe)
-        // update list to Live Data observers
-        _shoeList.value = aShoeList
+    lateinit var aShoe : Shoe
+
+    init {
+        clearShoe()
+    }
+
+    fun addShoeToList() {
+        Timber.i("adding a shoe name: ${aShoe.name} co: ${aShoe.company} size: ${aShoe.size} des: ${aShoe.description}")
+        if (aShoe.name.isBlank() || aShoe.size <= 0.0 ||
+            aShoe.company.isBlank() || aShoe.description.isBlank() ) {
+            // indicate to observers that shoe has blank fields
+            _showToast.value = true
+        } else {
+            // add new shoe to the list
+            aShoeList.add(aShoe)
+            // update list to Live Data observers
+            _shoeList.value = aShoeList
+            // indicate to observers that shoe was used to update list
+            _shoeComplete.value = true
+        }
+    }
+
+    fun cancelShoe() {
+        _shoeComplete.value = true
+    }
+
+    fun clearShoe() {
+        aShoe = Shoe("", 0.0, "", "")
+        _showToast.value = false
+        _shoeComplete.value = false
     }
 }
